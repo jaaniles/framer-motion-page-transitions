e8b4d5266d2c48be9ea26659a2112cf39597a4b7
Jaani Leskinen <jaani.leskinen@fraktio.fi>
2019-11-27 10:27:04 +0200

    Initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import { AnimatePresence, motion } from "framer-motion"
+import { Link } from "react-router-dom";
+
+import login from "./assets/1.svg"
+import main from "./assets/2.svg"
+
+const appStyle = {
+  maxWidth: 360,
+  margin: "64px auto"
+}
+
+const pageStyle = {
+  minHeight: 640,
+  background: "#FDFFFC",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  borderRadius: 8,
+  width: "100%",
+}
+
+const imgStyle = {
+  maxWidth: 500
+}
+
+const pageVariants = {
+  exit: {
+    opacity: 0,
+    scale: 0.8,
+  },
+  enter: {
+    opacity: 1,
+    scale: 1,
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={appStyle}>
+      <Router>
+        <Route
+          render={({ location }) => (
+            <AnimatePresence exitBeforeEnter>
+              <Switch location={location} key={location.pathname}>
+                <Route exact path="/" component={Login} />
+                <Route exact path="/main" component={Main} />
+              </Switch>
+            </AnimatePresence>
+          )}
+        />
+      </Router>
     </div>
   );
 }
 
-export default App;
+
+
+const Login = () => (
+  <motion.div 
+    initial="exit"
+    animate="enter"
+    exit="exit"
+    variants={pageVariants}
+    style={pageStyle} 
+  >
+    <img style={imgStyle} src={login} alt="1" />
+    <Link to="/main">
+      To Main
+    </Link>
+  </motion.div>
+)
+
+const Main = () => (
+  <motion.div 
+    initial="exit"
+    animate="enter"
+    exit="exit"
+    variants={pageVariants}
+    style={pageStyle} 
+  >
+    <img style={imgStyle} src={main} alt="2" />
+    <Link to="/">
+      To Login
+    </Link>
+  </motion.div>
+)
+
+export default App
